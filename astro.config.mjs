e720f307e355a52cddfdd6cf98db2dd75116f021
Carolina <carolinarb1994@gmail.com>
2026-02-04 21:01:27 +0100

    feat: actualizar configuración de Astro y mejorar el pie de página con nuevos íconos de redes sociales

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
@@ -6,4 +7,7 @@ export default defineConfig({
   build: {
     inlineStylesheets: 'auto',
   },
+  integrations: [
+    tailwind(),
+  ],
 });
